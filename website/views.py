cbe0d955d6c2f6c25347d980719020be77e7ef00
arturflacon <202414854+arturflacon@users.noreply.github.com>
2026-03-20 19:28:14 -0400

    Implementação das CreateViews para Cliente, Administrador, Chacara e Reserva

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
+from .models import Cliente, Administrador, Chacara, Reserva
 
 
 class IndexView(TemplateView):
@@ -10,3 +12,35 @@ class ContatoView(TemplateView):
 class SobreView(TemplateView):
     template_name = "website/sobre.html"
 
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = ['nome', 'telefone']  # 'usuario' será tratado em um fluxo de registro de usuário separado
+    template_name = 'website/cliente_form.html' # Você precisará criar este template
+    success_url = reverse_lazy('index') # Redireciona para a página inicial após o sucesso
+
+
+class AdministradorCreate(CreateView):
+    model = Administrador
+    fields = ['nome']  # 'usuario' será tratado em um fluxo de registro de usuário separado
+    template_name = 'website/administrador_form.html' # Você precisará criar este template
+    success_url = reverse_lazy('index')
+
+
+class ChacaraCreate(CreateView):
+    model = Chacara
+    fields = [
+        'nome', 'descricao', 'preco_diaria', 'tem_estacionamento',
+        'tem_piscina', 'tem_churrasqueira', 'num_quartos', 'num_banheiros'
+    ]
+    template_name = 'website/chacara_form.html' # Você precisará criar este template
+    success_url = reverse_lazy('index')
+
+
+class ReservaCreate(CreateView):
+    model = Reserva
+    fields = [
+        'cliente', 'chacara', 'data_inicio', 'data_fim', 'valor_total', 'status'
+    ]
+    template_name = 'website/reserva_form.html' # Você precisará criar este template
+    success_url = reverse_lazy('index')
