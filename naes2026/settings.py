e9dccb3b4db4fd6f1473e8aad3f708507b17f58c
arturflacon <flaconartur@gmail.com>
2026-06-26 16:39:08 -0300

    feat: google cloud integrado

--- naes2026/settings.py
+++ naes2026/settings.py
@@ -82,6 +82,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static_gcloud'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
